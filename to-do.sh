a1c0c9661f9e1eac4277618b368de6cd19033f90
Pablo Marti <pablojmarti@gmail.com>
2017-08-29 14:41:13 -0400

    Squashed commit of the following:
     Adds a way to use flags with script
    
         adds file variable; edits add task, ; adds a way to list tasks
    
         adds a function to create a file where the todo list will be written; as well as update spelling mistakes
    
         add a opt args while loop as well as moving functions

--- to-do.sh
+++ to-do.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+# variables 
+file="/tmp/todocli.txt"
+
+get_task(){
+  echo "getting task from file"
+}
+
 add_task(){
-  echo "add task"
+  echo "$1" >> "$file"
 }
 
 remove_task(){
@@ -9,5 +16,33 @@ remove_task(){
 }
 
 list_tasks(){
-  echo "here are all the tasks"
+  while read task;
+  do
+    echo $task
+  done <$file
 }
+
+while getopts ":a:r:l" opt; do
+  case $opt in
+    a)
+      echo "adding $OPTARG"
+      add_task "$OPTARG"
+      ;;
+    r)
+      echo "removing $OPTARG"
+      ;;
+    l)
+      echo "listing tasks"
+      list_tasks
+      ;;
+    \?)
+      echo "Invalid Option -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
